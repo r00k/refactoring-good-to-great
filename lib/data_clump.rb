e2388beefef385b06196d3fe9add3e96182abd7b
Ben Orenstein <ben.orenstein@gmail.com>
2012-06-24 14:42:42 -0400

    More

--- lib/data_clump.rb
+++ lib/data_clump.rb
@@ -2,14 +2,13 @@ require 'active_support_date_stuff'
 require 'ostruct'
 
 class OrdersReport
-
   def initialize(orders, start_date, end_date)
     @orders = orders
     @start_date = start_date
     @end_date = end_date
   end
 
-  def total_sales
+  def total_sales_within_range
     orders_in_range = @orders.select { |order| order.placed_at >= @start_date && order.placed_at <= @end_date }
     orders_in_range.map(&:amount).sum
   end
--- spec/data_clump_spec.rb
+++ spec/data_clump_spec.rb
@@ -1,13 +1,13 @@
 require_relative '../lib/data_clump'
 
 describe OrdersReport do
-  describe '#total_sales' do
+  describe '#total_sales_within_range' do
     it 'returns total sales in range' do
-      order_this_week = Order.new(amount: 5, placed_at: Date.today)
-      order_last_week = Order.new(amount: 5, placed_at: Date.today - 10.days)
-      orders = [order_this_week, order_last_week]
+      order_placed_this_week = Order.new(amount: 5, placed_at: Date.today)
+      order_placed_last_week = Order.new(amount: 5, placed_at: Date.today - 10.days)
+      orders = [order_placed_this_week, order_placed_last_week]
 
-      OrdersReport.new(orders, Date.today.beginning_of_week, Date.today.end_of_week).total_sales.should == 5
+      OrdersReport.new(orders, Date.today.beginning_of_week, Date.today.end_of_week).total_sales_within_range.should == 5
     end
   end
 end

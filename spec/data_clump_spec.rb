01ce6c10f545f391618c8528adcdc20ec479c497
Ben Orenstein <ben.orenstein@gmail.com>
2012-06-28 22:28:06 +0100

    Change test.

--- spec/data_clump_spec.rb
+++ spec/data_clump_spec.rb
@@ -3,11 +3,13 @@ require_relative '../lib/data_clump'
 describe OrdersReport do
   describe '#total_sales_within_range' do
     it 'returns total sales in range' do
-      order_placed_this_week = Order.new(amount: 5, placed_at: Date.new(2012, 1, 28))
-      order_placed_last_week = Order.new(amount: 5, placed_at: Date.new(2012, 1, 1))
-      orders = [order_placed_this_week, order_placed_last_week]
+      order_within_range = Order.new(amount: 5, placed_at: Date.new(2012, 1, 28))
+      order_out_of_range = Order.new(amount: 5, placed_at: Date.new(2012, 1, 1))
+      orders = [order_within_range, order_out_of_range]
 
-      OrdersReport.new(orders, Date.new(2012, 1, 15), Date.new(2012, 1, 30)).total_sales_within_range.should == 5
+      OrdersReport.
+        new(orders, Date.new(2012, 1, 15), Date.new(2012, 1, 30)).
+        total_sales_within_range.should == 5
     end
   end
 end
